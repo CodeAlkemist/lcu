51c5d02c7d971ebecec0756673a9839e8af43908
Matheus Xavier Silva <mxs@Matheus-note-deb.rad.codealkemy.co>
2018-08-16 21:33:56 -0300

    work on vector

--- src/vector.c
+++ src/vector.c
@@ -2,4 +2,23 @@
 // Created by mxs on 10/08/18.
 //
 
+#include <malloc.h>
 #include "vector.h"
+
+void lcu_vector_init(lcu_vector_t *self, unsigned int item_size, unsigned int step, unsigned int size_init)
+{
+    self->item_size = item_size;
+    self->contents = malloc(item_size*size_init);
+    if(!self->contents)
+    {
+        return;
+    }
+    self->caret = self->contents;
+    self->item_size = item_size;
+    self->step_size = step;
+    self->actual_size = size_init;
+    self->used_size = 0;
+    self->position = 0;
+    self->initialized = 1;
+    
+}
--- src/vector.h
+++ src/vector.h
@@ -8,12 +8,24 @@
 typedef struct __lcu_vector_base
 {
     void *contents;
+    void *caret;
     unsigned int item_size;
     unsigned int step_size;
     unsigned int actual_size;
     unsigned int used_size;
     unsigned int position;
+    char initialized;
 } lcu_vector_t;
+
+void lcu_vector_init(lcu_vector_t *, unsigned int, unsigned int, unsigned int);
+
+void lcu_vect_append(lcu_vector_t *, void *);
+
+void lcu_vect_add(lcu_vector_t *, void *, int position);
+
+
+
+
 
 
 
